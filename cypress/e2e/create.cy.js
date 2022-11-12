678d68c72bfa6ab404e8024dddab7831dcd8780d
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-11-12 18:00:29 +0100

    Added validation to create post

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -7,7 +7,7 @@ describe("Create a new post", () => {
   });
 
   it("CAN create post", () => {
-    // gets the currently showing close button
+    // gets the close button for the register form
     cy.get(".btn-close:visible").click();
     cy.wait(500);
     //gets the login button in header
@@ -32,5 +32,31 @@ describe("Create a new post", () => {
     cy.get("#postBody").should("exist").type("I'm so glad this is working");
     cy.get('button[data-action="submit"]:visible').click();
     cy.wait(5000);
+  });
+
+  it("Can validate inputs and validation messages", () => {
+    // gets the close button for the register form
+    cy.get(".btn-close:visible").click();
+    cy.wait(500);
+    //gets the login button in header
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(500);
+    cy.get("input[type='email']:visible")
+      .should("exist")
+      .type(`testersen@noroff.no`);
+    cy.get("input[type='password']:visible")
+      .should("exist")
+      .type(`Mypassword{enter}`);
+    cy.wait(2000);
+    cy.url().should("include", "profile");
+    cy.get(`a[href="/?view=post"]`).click();
+    cy.wait(1500);
+    cy.url().should("include", "post");
+    // submitting a empty form
+    cy.get('button[data-action="submit"]').click();
+    cy.get("#postTitle:invalid")
+      .invoke("prop", "validationMessage")
+      .should("exist");
+    cy.wait(200);
   });
 });
