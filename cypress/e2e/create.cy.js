28bf438cd126525f3f65f84fb2570cd2cff5c6e8
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-11-12 10:14:28 +0100

    devided the cy files

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -1,12 +1,12 @@
 const baseURL = "http://127.0.0.1:8080/";
 
-describe("Authenticated user login", () => {
+describe("Create a new post", () => {
   beforeEach(() => {
     cy.visit(baseURL);
     cy.clearLocalStorage();
   });
 
-  it("Can login", () => {
+  it("CAN create post", () => {
     // gets the currently showing close button
     cy.get(".btn-close:visible").click();
     cy.wait(500);
@@ -21,5 +21,16 @@ describe("Authenticated user login", () => {
       .type(`Mypassword{enter}`);
     cy.wait(2000);
     cy.url().should("include", "profile");
+    cy.get(`a[href="/?view=post"]`).click();
+    cy.wait(1500);
+    cy.url().should("include", "post");
+    cy.get("#postTitle").should("exist").type("This is a Cypresstest");
+    cy.get("#postTags").should("exist").type("test, cypress");
+    cy.get("#postMedia")
+      .should("exist")
+      .type("https://picsum.photos/id/25/367/267");
+    cy.get("#postBody").should("exist").type("I'm so glad this is working");
+    cy.get('button[data-action="submit"]:visible').click();
+    cy.wait(5000);
   });
 });
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,12 +1,12 @@
 const baseURL = "http://127.0.0.1:8080/";
 
-describe("Authenticated user login", () => {
+describe("Authenticated user logout", () => {
   beforeEach(() => {
     cy.visit(baseURL);
     cy.clearLocalStorage();
   });
 
-  it("Can login", () => {
+  it("CAN logout", () => {
     // gets the currently showing close button
     cy.get(".btn-close:visible").click();
     cy.wait(500);
@@ -21,5 +21,10 @@ describe("Authenticated user login", () => {
       .type(`Mypassword{enter}`);
     cy.wait(2000);
     cy.url().should("include", "profile");
+    cy.get("button[data-auth='logout']:visible")
+      .click({ force: true })
+      .should(() => {
+        expect(localStorage.getItem("token")).to.be.null;
+      });
   });
 });
